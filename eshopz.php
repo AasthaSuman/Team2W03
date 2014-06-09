f17e3692eef434989d39c662700eaa2f27556dae
AasthaSuman <aastha.suman717@gmail.com>
2014-06-09 20:15:40 +0530

    modifying files

---
+++ eshopz.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="uff-8">
+   <title>
+     Welcome to the world of e-shopping
+	 </title>
+	 <link type="text/css" rel="stylesheet" href="stylesheet.css"
+	 </head>
+     <body>
+	 <div class="header"> 
+        <form action="header.php" method="post">
+				  <pre>                                    Username :                        Password : </pre>
+				  <pre>
+				  <input type="text" name="username" value="">    <input type="password" name="secret" value="">   <input type="submit" value="Log in"> </pre>
+				  </form>
+				  </div>			
+<img src="images/9219.png" alt="eshop" width="500px" height="500px">
+     <div class="form">
+	  <h2> SIGN UP </h2>
+		<form action="header.php" method="post">
+		    <div class="table-row"> <p>First Name : </p> <input type="text" name="first name" value=""> </p> </div>
+			<div class="table-row"> <p>Last Name : </p> <input type="text" name="last name" value=""> </p> </div>			
+			<div class="table-row"> <p>USERNAME : </p> <p><input type="text" name="username" value=""> </p> </div>
+			<div class="table-row"> <p>Enter Password : </p> <p><input type="password" name="secret" value=""><br> </p> </div>
+			<div class="table-row"> <p>Re-enter Password : </p> <p><input type="password" name="secret" value=""> </p> </div>
+			<div class="table-row"> <p>E-mail Id : </p> <p><input type="email" name="email" value=""></p> </div>
+			<div class="table-row"> <p>Gender : </p> <p><input type="radio" name="Gender" value="Female"> Female
+			<input type="radio" name="Gender" value="Male"> Male </p> </div>
+			<input type="submit" value="Sign Up">
+			</form>
+			</div>
+	 </body>	 
+     	  </html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+$username=$_POST['username'];
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>
+e-Shopz
+</title>
+ <link type="text/css" rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<div class="header">
+<p>
+<?php echo 'Welcome to eshopz, ',$username,'' ?>
+</p>
+</div>
+</body>
+</html>
